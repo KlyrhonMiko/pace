7b5da67786dd03f8a7f59ffa844263fdab0e84a1
Klyrhon <139136307+KlyrhonMiko@users.noreply.github.com>
2026-04-20 15:07:23 +0800

    fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,4 +1,8 @@
+import dotenv from "dotenv";
+import { join } from "path";
 import type { NextConfig } from "next";
+
+dotenv.config({ path: join(process.cwd(), "../.env.local") });
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["pdf-parse"],
